04b61e3d7bab5831e0b29ce280e2f47c830e6016
TysonMcLaws <47451713+TysonMcLaws@users.noreply.github.com>
2019-11-02 17:20:25 -0500

    Create control.sh

---
+++ control.sh
@@ -0,0 +1,19 @@
+echo "Thank-you for testing my program"
+
+if [ "`echo -n`" = "-n" ]; then
+  n=""
+  c="\c"
+else
+  n="-n"
+  c=""
+fi
+
+echo $n For future refrence, what is your name? $c
+read name
+echo "Hello, $name"
+
+echo "Installing termuxStartPython..."
+echo "This may take a long time..."
+
+apt-get wget
+wget 
